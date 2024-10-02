acccb783db1d43562a63631989d62e1139d3f8fd
Marija Sljukic <sljukic.marija22@gmail.com>
2024-10-02 16:08:31 +0200

    negative_number

--- functions.c
+++ functions.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
-//#include <cs50.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "functions.h"
-
 static bool char_valid(char c, char valid_char[])
 {
     int size = strlen(valid_char);
-    int i = 0;
-
-    for (i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         if (c == valid_char[i])
         {
             return true;
         }
     }
-
     return false;
 }
 
-
-
 bool exp_valid(char *str, char valid_char[])
 {
-    int lenght = strlen(str);
-    int i;
-    for (i = 0; i < lenght; i++)
+    size_t lenght = strlen(str);
+    for (size_t i = 0; i < lenght; i++)
     {
-        if (isspace((unsigned char)str[i]))
+        if (isspace((int)str[i]))
         {
             return false;
         }
         if (!char_valid(str[i], valid_char))
         {
-        return false;
+            return false;
         }
     }
     return true;
 }
 
-
 static char is_operator(char c)
 {
-    char valid_operator[]= "+-*/()";
+    char valid_operator[] = "+-*/()";
     int lenght = strlen(valid_operator);
-    int i = 0;
-    for (i = 0; i < lenght; i++)
+    for (int i = 0; i < lenght; i++)
     {
         if (c == valid_operator[i])
         {
@@ -59,16 +48,14 @@ static char is_operator(char c)
     return 0;
 }
 
-
-static int find_matching_parenthesis(char *str, int open_index)
+static int find_matching_parenthesis(char *str, int close_index)
 {
     int count = 1;
-    int i;
-    for (i = open_index + 1; i < strlen(str); i++)
+    for (int i = close_index - 1; i >= 0; i--)
     {
-        if (str[i] == '(')
+        if (str[i] == ')')
             count++;
-        else if (str[i] == ')')
+        else if (str[i] == '(')
             count--;
         if (count == 0)
             return i;
@@ -78,23 +65,23 @@ static int find_matching_parenthesis(char *str, int open_index)
 
 int calculate_exp(char *str)
 {
+    int lenght = strlen(str);
     int i = 0;
     int j = 0;
     int nizOperanada[10];
     char nizOperatora[10];
     int number = 0;
-    int lenght = strlen(str);
     int operator_counter = 0;
     int operanda_counter = 0;
 
-//determing parenthesis
-     while (i < lenght)
-     {
-        if (str[i] == '(')
+    // Obrada zagrada: prvo tražimo zatvorene zagrade
+    for (i = 0; i < lenght; i++)
+    {
+        if (str[i] == ')')
         {
-            int open_index = i;
-            int close_index = find_matching_parenthesis(str, open_index);
-            if (close_index == -1)
+            int close_index = i;
+            int open_index = find_matching_parenthesis(str, close_index);
+            if (open_index == -1)
             {
                 printf("Mismatched parentheses\n");
                 return -1;
@@ -102,74 +89,80 @@ int calculate_exp(char *str)
 
             char *between = calloc(50, sizeof(char));
             strncpy(between, str + open_index + 1, close_index - open_index - 1);
+            between[close_index - open_index - 1] = '\0';
 
             int sub_result = calculate_exp(between);
-
             free(between);
 
-            char *result_str = calloc(20, sizeof(char));
-            sprintf(result_str, "%d", sub_result);
-
+            // Zamena izraza unutar zagrada rezultatom
             char *new_str = calloc(50, sizeof(char));
             strncpy(new_str, str, open_index);
-            strcat(new_str, result_str);
+            char sub_result_str[20];
+            sprintf(sub_result_str, "%d", sub_result);
+            strcat(new_str, sub_result_str);
             strcat(new_str, str + close_index + 1);
 
-            free(result_str);
-
             int final_result = calculate_exp(new_str);
-
             free(new_str);
 
             return final_result;
-
-            //free(between);
-            //free(result_str);
         }
-        i++;
-
     }
-//determining operande i operatore
+
+    // Parsiranje operanda i operatora kada nema više zagrada
     for (i = 0; i < lenght; i++)
     {
-        //check and get numbers
-        if (isdigit((unsigned char)str[i]))
+        // Obrada brojeva, uključujući negativne brojeve
+        if (isdigit((int)str[i]) || (str[i] == '-' && (i == 0 || is_operator(str[i - 1]))))
         {
-            number = number * 10 + (str[i] - '0');
-            if (i == lenght - 1 || !isdigit((unsigned char)str[i+1]))
+            bool is_negative = false;
+            if (str[i] == '-')
             {
-                nizOperanada[operanda_counter] = number;
-                operanda_counter++;
-
+                is_negative = true;
+                i++;
             }
+
+            number = 0;  // Reset broj za svaki broj
+            while (i < lenght && isdigit((int)str[i]))
+            {
+                number = number * 10 + (str[i] - '0');
+                i++;
+            }
+            i--;  // Vrati indeks jer će petlja povećati
+
+            if (is_negative)
+            {
+                number = -number;
+            }
+
+            nizOperanada[operanda_counter] = number;
+            operanda_counter++;
         }
-        //what to do if char is operator
+        // Obrada operatora
         else if (is_operator(str[i]))
         {
-            char operator = str[i];
-            //nizOperanada[operanda_counter] = number;
-            nizOperatora[operator_counter] = operator;
+            nizOperatora[operator_counter] = str[i];
             operator_counter++;
-            number = 0;
         }
         else
         {
-            printf("Shouldn't be here...\n");
+            printf("Invalid character found...\n");
             return 1;
         }
     }
+
+    // Obrada operatora sa višim prioritetom (* i /)
     for (i = 0; i < operator_counter; i++)
     {
         if (nizOperatora[i] == '*' || nizOperatora[i] == '/')
         {
             if (nizOperatora[i] == '*')
             {
-                nizOperanada[i] = nizOperanada[i] * nizOperanada[i+1];
+                nizOperanada[i] = nizOperanada[i] * nizOperanada[i + 1];
             }
             else if (nizOperatora[i] == '/')
             {
-                nizOperanada[i] = nizOperanada[i] / nizOperanada[i+1];
-
+                nizOperanada[i] = nizOperanada[i] / nizOperanada[i + 1];
             }
 
             for (j = i + 1; j < operator_counter; j++)
@@ -183,17 +176,18 @@ int calculate_exp(char *str)
         }
     }
 
+    // Obrada operatora sa nižim prioritetom (+ i -)
     int result = nizOperanada[0];
     for (i = 0; i < operator_counter; i++)
     {
         if (nizOperatora[i] == '-')
-            {
-                result -= nizOperanada[i + 1];
-            }
+        {
+            result -= nizOperanada[i + 1];
+        }
         else if (nizOperatora[i] == '+')
-            {
-                result += nizOperanada[i + 1];
-            }
+        {
+            result += nizOperanada[i + 1];
+        }
     }
 
     return result;
