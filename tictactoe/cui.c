5425cdbfdd97bb6c891ff2c59b823c0b1e89e650
Marija Sljukic <sljukic.marija22@gmail.com>
2024-10-03 13:25:09 +0200

    show_board

---
+++ tictactoe/cui.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "const.h"
+
+#define SIZE SIDE
+
+void instructions(){
+    printf("Chose a cell numbered from 1 to 9 as below and play");
+   printf("\n");
+    printf("\t\t\t 1 | 2 | 3 \t\t\t\n \t\t\t 4 | 5 | 6\t\t\t\n \t\t\t 7 | 8 | 9 \t\t\t\n");
+    printf("\n");
+}
+
+void show_board(char board[SIZE][SIZE])
+{
+   
+  
+     printf("\n\t\t\t %c | %c | %c \t\t\t\n \t\t\t %c | %c | %c\t\t\t\n \t\t\t %c | %c | %c \t\t\t\n", board[0][0], board[0][1], board[1][2],board[1][0], board[1][1],board[1][2],board[0][2],board[1][2],board[2][2]);
+   
+}
+
+
