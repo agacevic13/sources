627dc1a8a904a6992d4f1bc7110410d4092fbcd9
Marija Sljukic <sljukic.marija22@gmail.com>
2024-10-03 10:37:38 +0200

    tictactoe_start

---
+++ tictactoe/build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/main.c.o"
+  "CMakeFiles/tictactoe.dir/main.c.o.d"
+  "libtictactoe.dll.a"
+  "tictactoe.exe"
+  "tictactoe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
