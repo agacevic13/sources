badf2e60883ba7224d319a6eff84a26d97dc2064
Anica Gacevic <anicagacevic@gmail.com>
2024-10-07 10:26:00 +0200

    computer medium

--- tictactoe/computer.c
+++ tictactoe/computer.c
@@ -4,149 +4,153 @@
 #include "const.h"
 #include <stdbool.h>
 
-bool  is_empty(int move, char board[SIDE][SIDE])
+bool is_empty(int move, char board[SIDE][SIDE])
 {
-int row = (move-1)/SIDE;
-int column = (move-1)%SIDE;
-if(board[row][column]==' '){
-    return true;
-}else{
-    return false;
-}
+    int row = (move-1)/SIDE;
+    int column = (move-1)%SIDE;
+    if(board[row][column]==' ')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-int finish(char board[SIDE][SIDE]){
-    //checking lines
-for(int i=0; i< SIDE; i++){
-    if(board[i][0]==COMPUTER_SYMBOL && board[i][1]== COMPUTER_SYMBOL && board[i][2] == ' '){
-        return (i*SIDE+3);
+int finish(char board[SIDE][SIDE])
+{
+    //checking rows
+    for(int i=0; i< SIDE; i++)
+    {
+        if(board[i][0]==COMPUTER_SYMBOL && board[i][1]== COMPUTER_SYMBOL && board[i][2] == ' '){
+            return (i*SIDE+3);
+        }
+        if(board[i][0]==COMPUTER_SYMBOL && board[i][2]== COMPUTER_SYMBOL && board[i][1] == ' '){
+            return (i*SIDE+2);
+        }
+        if(board[i][1]==COMPUTER_SYMBOL && board[i][2]== COMPUTER_SYMBOL && board[i][0] == ' '){
+            return (i*SIDE+1);
+        }
     }
-     if(board[i][0]==COMPUTER_SYMBOL && board[i][2]== COMPUTER_SYMBOL && board[i][1] == ' '){
-        return (i*SIDE+2);
+    //checking columns
+    for(int i=0; i<SIDE; i++)
+    {
+        if(board[0][i]==COMPUTER_SYMBOL && board[1][i]==COMPUTER_SYMBOL && board[2][i] == ' '){
+            return (7+i); 
+        }
     }
-      if(board[i][1]==COMPUTER_SYMBOL && board[i][2]== COMPUTER_SYMBOL && board[i][0] == ' '){
-        return (i*SIDE+1);
+    for(int i=0; i<SIDE; i++){
+        if(board[1][i]==COMPUTER_SYMBOL && board[2][i]==COMPUTER_SYMBOL && board[0][i] == ' '){
+            return i;
+        }
     }
-}
-//checking rows
-for(int i=0; i<SIDE; i++){
-    if(board[0][i]==COMPUTER_SYMBOL && board[1][i]==COMPUTER_SYMBOL && board[3][i] == ' '){
-        return (6+i);
+    for(int i=0; i<SIDE; i++){
+        if(board[0][i]==COMPUTER_SYMBOL && board[2][i]==COMPUTER_SYMBOL && board[1][i] == ' '){
+            return (4+i);
+        }
     }
-}
-for(int i=0; i<SIDE; i++){
-    if(board[1][i]==COMPUTER_SYMBOL && board[2][i]==COMPUTER_SYMBOL && board[0][i] == ' '){
-        return i;
+    //checking diagonals
+    if(board[0][0]==board[1][1] && board[0][0]==COMPUTER_SYMBOL && board[2][2]==' '){
+        return 9;
     }
-}
-for(int i=0; i<SIDE; i++){
-    if(board[0][i]==COMPUTER_SYMBOL && board[2][i]==COMPUTER_SYMBOL && board[1][i] == ' '){
-        return (3+i);
+    else if(board[0][0]==' ' && board[1][1]==COMPUTER_SYMBOL && board[2][2]==board[1][1]){
+        return 1;
     }
-}
-//checking diagonals
-if(board[0][0]==board[1][1] && board[0][0]==COMPUTER_SYMBOL && board[2][2]==' '){
-    return 9;
-}
-else if(board[0][0]==' ' && board[1][1]==COMPUTER_SYMBOL && board[2][2]==board[1][1]){
-    return 1;
-}
-else if((board[0][0]==board[2][2] && board[0][0]==COMPUTER_SYMBOL && board[1][1]==' ') || (board[0][2]==board[2][0] && board[0][2]==COMPUTER_SYMBOL && board[1][1]==' ')){
-    return 5;
-}
-else if(board[0][2]==COMPUTER_SYMBOL && board[0][2]==board[1][1] && board[0][2]==' '){
-    return 7;
-}
-if(board[0][2]==' ' && board[1][1]==COMPUTER_SYMBOL && board[0][2]==board[1][1]){
-     return 3;
-     }
-return 0;
+    else if((board[0][0]==board[2][2] && board[0][0]==COMPUTER_SYMBOL && board[1][1]==' ') || (board[0][2]==board[2][0] && board[0][2]==COMPUTER_SYMBOL && board[1][1]==' ')){
+        return 5;
+    }
+    else if(board[0][2]==COMPUTER_SYMBOL && board[0][2]==board[1][1] && board[2][0]==' '){
+        return 7;
+    }
+    if(board[0][2]==' ' && board[1][1]==COMPUTER_SYMBOL && board[0][2]==board[1][1]){
+        return 3;
+        }
+    return 0;
 }
 
 
 int defence(char board[SIDE][SIDE]){
     //checking lines
-for(int i=0; i< SIDE; i++){
-    if(board[i][0]==PLAYER1_SYMBOL && board[i][1]== PLAYER1_SYMBOL && board[i][2] == ' '){
-        return (i*SIDE+3);
+    for(int i=0; i< SIDE; i++){
+        if(board[i][0]==PLAYER1_SYMBOL && board[i][1]== PLAYER1_SYMBOL && board[i][2] == ' '){
+            return (i*SIDE+3);
+        }
+        if(board[i][0]==PLAYER1_SYMBOL && board[i][2]== PLAYER1_SYMBOL && board[i][1] == ' '){
+            return (i*SIDE+2);
+        }
+        if(board[i][1]==PLAYER1_SYMBOL && board[i][2]== PLAYER1_SYMBOL && board[i][0] == ' '){
+            return (i*SIDE+1);
+        }
     }
-     if(board[i][0]==PLAYER1_SYMBOL && board[i][2]== PLAYER1_SYMBOL && board[i][1] == ' '){
-        return (i*SIDE+2);
+    //checking rows
+    for(int i=0; i<SIDE; i++){
+        if(board[0][i]==PLAYER1_SYMBOL && board[1][i]==PLAYER1_SYMBOL && board[2][i] == ' '){
+            return (7+i);
+        }
     }
-      if(board[i][1]==PLAYER1_SYMBOL && board[i][2]== PLAYER1_SYMBOL && board[i][0] == ' '){
-        return (i*SIDE+1);
+    for(int i=0; i<SIDE; i++){
+        if(board[1][i]==PLAYER1_SYMBOL && board[2][i]==PLAYER1_SYMBOL && board[0][i] == ' '){
+            return i;
+        }
     }
-}
-//checking rows
-for(int i=0; i<SIDE; i++){
-    if(board[0][i]==PLAYER1_SYMBOL && board[1][i]==PLAYER1_SYMBOL && board[3][i] == ' '){
-        return (6+i);
+    for(int i=0; i<SIDE; i++){
+        if(board[0][i]==PLAYER1_SYMBOL && board[2][i]==PLAYER1_SYMBOL && board[1][i] == ' '){
+            return (4+i);
+        }
     }
-}
-for(int i=0; i<SIDE; i++){
-    if(board[1][i]==PLAYER1_SYMBOL && board[2][i]==PLAYER1_SYMBOL && board[0][i] == ' '){
-        return i;
+    //checking diagonals
+    if(board[0][0]==board[1][1] && board[0][0]==PLAYER1_SYMBOL && board[2][2]==' '){
+        return 9;
     }
-}
-for(int i=0; i<SIDE; i++){
-    if(board[0][i]==PLAYER1_SYMBOL && board[2][i]==PLAYER1_SYMBOL && board[1][i] == ' '){
-        return (3+i);
+    else if(board[0][0]==' ' && board[1][1]==PLAYER1_SYMBOL && board[2][2]==board[1][1]){
+        return 1;
     }
-}
-//checking diagonals
-if(board[0][0]==board[1][1] && board[0][0]==PLAYER1_SYMBOL && board[2][2]==' '){
-    return 9;
-}
-else if(board[0][0]==' ' && board[1][1]==PLAYER1_SYMBOL && board[2][2]==board[1][1]){
-    return 1;
-}
-else if((board[0][0]==board[2][2] && board[0][0]==PLAYER1_SYMBOL && board[1][1]==' ') || (board[0][2]==board[2][0] && board[0][2]==PLAYER1_SYMBOL && board[1][1]==' ')){
-    return 5;
-}
-else if(board[0][2]==PLAYER1_SYMBOL && board[0][2]==board[1][1] && board[0][2]==' '){
-    return 7;
-}
-if(board[0][2]==' ' && board[1][1]==PLAYER1_SYMBOL && board[0][2]==board[1][1]){
-     return 3;
-     }
-return 0;
+    else if((board[0][0]==board[2][2] && board[0][0]==PLAYER1_SYMBOL && board[1][1]==' ') || (board[0][2]==board[2][0] && board[0][2]==PLAYER1_SYMBOL && board[1][1]==' ')){
+        return 5;
+    }
+    else if(board[0][2]==PLAYER1_SYMBOL && board[0][2]==board[1][1] && board[2][0]==' '){
+        return 7;
+    }
+    if(board[0][2]==' ' && board[1][1]==PLAYER1_SYMBOL && board[0][2]==board[1][1]){
+        return 3;
+        }
+    return 0;
 }
 
+ int computer_move_simple(char board[SIDE][SIDE])
+ {
+    static int initialized = 0;
+    if (!initialized) 
+    {
+        srand(time(NULL));  
+        initialized = 1;     
+    }
+     while(1)
+    {
+        int move = (rand()%(SIDE*SIDE))+1;
+        if(is_empty(move, board))
+        {
+            return move;
+        }
+    }
+ }
 
 
-
-
-int computer_move(char board[SIDE][SIDE]){
+int computer_move(char board[SIDE][SIDE])
+{
     int move = 0;
-   if(finish(board)!=0){
-      move=finish;
-   }
-   else if(defence(board)!=0){
-    move = defence;
-   }
-   else{
-    move = computer_move_simple(board);
-   }
+    if(finish(board)!=0){
+        move = finish(board);
+    }
+    else if(defence(board)!=0){
+        move = defence(board);
+    }
+    else{
+        move = computer_move_simple(board);
+    }
     return move;
 
 }
-
- int computer_move_simple(char board[SIDE][SIDE]){
-   
-       static int initialized = 0;
-    if (!initialized) {
-        srand(time(NULL));  
-       initialized = 1;     
-    }
-
- while(1){
-     int move = (rand()%(SIDE*SIDE))+1;
-     if(is_empty(move, board)){
-        return move;
-     }
-
-
- }
 
 
 #ifdef TEST
