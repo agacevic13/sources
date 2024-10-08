4f76925a8554ce47f251137eb2e5b3904ac99f21
Anica Gacevic <anicagacevic@gmail.com>
2024-10-08 15:57:27 +0200

    play function

--- tictactoe9x9/ninexnine.c
+++ tictactoe9x9/ninexnine.c
@@ -63,127 +63,92 @@ void print_bigBoard(struct smallBoard bigBoard[SIDE])
         }
         //printf("\n");
         printf("\n\n");
+}
+
+bool play(int *bigmove, int *whoseTurn)
+{
+    const int player = *whoseTurn;
+    int smallmove;
+    printf("Player %d, input a number 1-9, where you want to place your symbol? ", player);
+    scanf("%d",&smallmove);
+    int row = (smallmove - 1) / INNER_SIZE;
+    int column = (smallmove - 1) % INNER_SIZE;
+    if (smallmove < 1 || smallmove > 9)
+    {
+        printf("Invalid input, please try again.\n");
+        return false;
     }
+    else if (bigBoard[*bigmove].board[row][column] != ' ')
+    {
+        printf("Field already taked. Try another.\n");
+        return false; 
+    }
+    else
+    {
+        char symbol = PLAYER1_SIMBOL;
+        if (player == PLAYER2)
+        {
+            symbol = PLAYER2_SIMBOL;
+        }
+        bigBoard[*bigmove].board[row][column] = symbol;
+        print_bigBoard(bigBoard);
+        //check win
+        small_win(bigBoard, *bigmove);
+        if (bigWinner(bigBoard) == true)
+        {
+            //printf("%c", whoIsWinner(bigBoard));
+            char win = whoIsWinner(bigBoard);
+            if(win == 'X')
+            {
+                printf("PLAYER 1 WINS!");
+            }
+            else if (win == 'O')
+            {
+                printf("PLAYER 2 WINS!");
+            }
+            else{
+                printf("IT'S A TIE!");
+            }
+        }
+        else
+        {
+            next_table(bigBoard, smallmove);
+            if (player == PLAYER1)
+            {
+                *whoseTurn = PLAYER2;
+            }
+            else
+            {
+                *whoseTurn = PLAYER1;
+            }
+            for (int i = 0; i < SIDE; i++)
+            {
+                if (bigBoard[i].active == true)
+                {
+                    *bigmove = i;
+                }
+            
+            }
+        }
+        return true;
+    }
+}
 #if 1
 void convert(int whoseTurn)
 {
     int move;
-    int smallmove;
     printf("Input a number 1-9 in which small board you want to start? ");
     scanf("%d",&move);
     int bigmove = move -1;
     while (bigWinner(bigBoard) == false)
     {
-        if (whoseTurn == PLAYER1)
+        if (whoseTurn == PLAYER1 || whoseTurn == PLAYER2)
         {
-            printf("Player 1, input a number 1-9, where you want to place your symbol? ");
-            scanf("%d",&smallmove);
-            int row = (smallmove - 1) / INNER_SIZE;
-            int column = (smallmove - 1) % INNER_SIZE;
-            if (smallmove < 1 || smallmove > 9)
+            if (play(&bigmove, &whoseTurn) == false)
             {
-                printf("Invalid input, please try again.\n");
                 continue;
             }
-            else if (bigBoard[bigmove].board[row][column] != ' ')
-            {
-                printf("Field already taked. Try another.\n");
-                continue; 
-            }
-            else
-            {
-                bigBoard[bigmove].board[row][column] = PLAYER1_SIMBOL;
-                print_bigBoard(bigBoard);
-                //check win
-                small_win(bigBoard, bigmove);
-                if (bigWinner(bigBoard) == true)
-                {
-                    //printf("%c", whoIsWinner(bigBoard));
-                    char win = whoIsWinner(bigBoard);
-                    if(win == 'X')
-                    {
-                        printf("PLAYER 1 WINS!");
-                    }
-                    else if (win == 'O')
-                    {
-                        printf("PLAYER 2 WINS!");
-                    }
-                    else{
-                        printf("IT'S A TIE!");
-                    }
-                }
-                else
-                {
-                    next_table(bigBoard, smallmove);
-                    whoseTurn = PLAYER2; 
-                    for (int i = 0; i < SIDE; i++)
-                    {
-                        if (bigBoard[i].active == true)
-                        {
-                            bigmove = i;
-                        }
-                    
-                    }
-                
-                }
-            }
-
         }
-        else if(whoseTurn == PLAYER2)
-        {
-            printf("Player 2, input a number 1-9, where you want to place your symbol? ");
-            scanf("%d",&smallmove);
-            int row = (smallmove - 1) / INNER_SIZE;
-            int column = (smallmove - 1) % INNER_SIZE;
-            if (smallmove < 1 || smallmove > 9)
-            {
-                printf("Invalid input, please try again.\n");
-                continue;
-            }
-            else if (bigBoard[bigmove].board[row][column] != ' ')
-            {
-                printf("Field already taked. Try another.\n");
-                continue; 
-            }
-            else
-            {
-                bigBoard[bigmove].board[row][column] = PLAYER2_SIMBOL;
-                print_bigBoard(bigBoard);
-                //check win
-                small_win(bigBoard, bigmove);
-                if (bigWinner(bigBoard) == true)
-                {
-                    //("%c", whoIsWinner(bigBoard));
-                    char win = whoIsWinner(bigBoard);
-                    if(win == 'X')
-                    {
-                        printf("PLAYER 1 WINS!");
-                    }
-                    else if (win == 'O')
-                    {
-                        printf("PLAYER 2 WINS!");
-                    }
-                    else{
-                        printf("IT'S A TIE!");
-                    }
-                }
-                else
-                {
-                    next_table(bigBoard, smallmove);
-                    whoseTurn = PLAYER1; 
-                    for (int i = 0; i < SIDE; i++)
-                    {
-                        if (bigBoard[i].active == true)
-                        {
-                            bigmove = i;
-                        }
-                    
-                    }
-                
-                }
-            }
-        }
-
     }
 }
 
