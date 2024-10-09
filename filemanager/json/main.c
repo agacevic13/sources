86ae60803956ecf13cc18a4056adae92c742ebe9
Anica Gacevic <anicagacevic@gmail.com>
2024-10-09 16:28:05 +0200

    sensors try

--- filemanager/json/main.c
+++ filemanager/json/main.c
@@ -1,6 +1,112 @@
 #include <stdio.h> 
 #include "cJSON.h"
 
+struct sensorData
+{
+    int ID;
+    char *room;
+    int temp;
+};
+struct sensorData mySD;
+
+cJSON *simulation = NULL;
+cJSON *sensors = NULL;
+cJSON *sensor = NULL;
+cJSON *ID = NULL;
+cJSON *room = NULL;
+cJSON *temp = NULL;
+
+void initSD(void)
+{
+    sensors = cJSON_CreateObject();
+}
+
+void sendSensorData(const struct sensorData* const sd)
+{
+    cJSON *simulation =cJSON_CreateObject();
+
+    sensors =cJSON_CreateArray();
+    cJSON_AddItemToObject(simulation, "sensors", sensors);
+    
+    sensor = cJSON_CreateObject();
+    cJSON_AddItemToArray(sensors, sensor);
+    ID = cJSON_CreateNumber(sd->ID);
+    cJSON_AddItemToObject(sensor, "ID", ID);
+
+    room = cJSON_CreateString(sd->room);
+    cJSON_AddItemToObject(sensor, "room", room);
+
+    temp = cJSON_CreateNumber(sd->temp);
+    cJSON_AddItemToObject(sensor, "temp", temp);
+
+    
+    char *jsonString = cJSON_Print(simulation);
+    printf("%s\n", jsonString);
+
+}
+
+#if 0
+void writeSensorData()
+{
+    FILE *fp = fopen("sensorData.json", "w");
+    if(fp == NULL)
+    {
+        printf("Error: Unable to open file.\n");
+        return 1;
+    }
+
+    
+    fputs(jsonString, fp);
+    fclose;
+
+}
+#endif
+
+
+#include <windows.h>
+
+int main()
+{
+    initSD();
+    
+    mySD.ID = 1;
+    mySD.room = "Kitchen";
+    mySD.temp = 23;
+
+    while(1)
+    {
+        sendSensorData(&mySD);
+        mySD.ID++;
+        Sleep(1000); // ms
+        if (mySD.ID == 3)
+        {
+            mySD.ID = 1;
+            mySD.temp+=3;
+            
+        }
+        if(mySD.ID == 2)
+        {
+            mySD.room = "Bedroom";
+            mySD.temp ++;
+        }
+        if (mySD.temp > 30) 
+        {
+            break;
+        }
+        
+#if 0
+        if(mySD.ID == 1)
+        {
+            
+        }
+#endif
+            
+    }
+}
+
+
+
+#if 0
 int createfile() { 
 // create a cJSON object 
 cJSON *json = cJSON_CreateObject(); 
@@ -25,3 +131,4 @@ cJSON_free(json_str);
 cJSON_Delete(json); 
 return 0; 
 }
+#endif
