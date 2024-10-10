f96123b196f325afdd50560c03428e266cbe0acd
Marija Sljukic <sljukic.marija22@gmail.com>
2024-10-10 13:51:07 +0200

    header_input

--- filemanager/compressor/input.c
+++ filemanager/compressor/input.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include "miniz.h"
+#include "input.h"
 #include <stdlib.h>
 
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint;
 
 void Load(char* filename, uint8* pOutBuff,uLong uncomp_len)   //arg: 
 {
