06e37ee76321ef7bcd093704bc5ae87d1b57937a
Anica Gacevic <anicagacevic@gmail.com>
2024-10-24 15:39:08 +0200

    uart Queue

---
+++ esp1/main/uartQueue.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "string.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+
+#define TXD_PIN 4
+#define RXD_PIN 5
+#define TX_BUF_SIZE 1024
+#define RX_BUF_SIZE 1024
+#define PATTERN_LEN 3
+#define TAG "UART"
+
+QueueHandle_t uart_queue;
+
+void uart_event_task(void *params)
+{
+  uart_event_t uart_event;
+  uint8_t *received_buffer = malloc(RX_BUF_SIZE);
+  size_t datalen;
+  while (true)
+  {
+    if (xQueueReceive(uart_queue, &uart_event, portMAX_DELAY))
+    {
+      switch (uart_event.type)
+      {
+      case UART_DATA:
+        ESP_LOGI(TAG, "UART_DATA");
+        uart_read_bytes(UART_NUM_1, received_buffer, uart_event.size, portMAX_DELAY);
+        printf("received: %.*s\n", uart_event.size, received_buffer);
+        break;
+      case UART_BREAK:
+        ESP_LOGI(TAG, "UART_BREAK");
+        break;
+      case UART_BUFFER_FULL:
+        ESP_LOGI(TAG, "UART_BUFFER_FULL");
+        break;
+      case UART_FIFO_OVF:
+        ESP_LOGI(TAG, "UART_FIFO_OVF");
+        uart_flush_input(UART_NUM_1);
+        xQueueReset(uart_queue);
+        break;
+      case UART_FRAME_ERR:
+        ESP_LOGI(TAG, "UART_FRAME_ERR");
+        break;
+      case UART_PARITY_ERR:
+        ESP_LOGI(TAG, "UART_PARITY_ERR");
+        break;
+      case UART_DATA_BREAK:
+        ESP_LOGI(TAG, "UART_DATA_BREAK");
+        break;
+      case UART_PATTERN_DET:
+        ESP_LOGI(TAG, "UART_PATTERN_DET");
+        uart_get_buffered_data_len(UART_NUM_1, &datalen);
+        int pos = uart_pattern_pop_pos(UART_NUM_1);
+        ESP_LOGI(TAG, "Detected %d pos %d", datalen, pos);
+        uart_read_bytes(UART_NUM_1, received_buffer, datalen - PATTERN_LEN, pdMS_TO_TICKS(1000));
+        uint8_t pat[PATTERN_LEN + 1];
+        memset(pat, 0, sizeof(pat));
+        uart_read_bytes(UART_NUM_1, pat, PATTERN_LEN, pdMS_TO_TICKS(1000));
+        printf("data: %.*s === pattern: %s\n", datalen - PATTERN_LEN, received_buffer, pat);
+        break;
+      default:
+        break;
+      }
+    }
+  }
+}
+
+#if 0
+QueueHandle_t uart_queue;
+
+void uart_event_task(void *params)
+{
+    uart_event_t uart_event;
+    uint8_t *received_buffer = malloc(RX_BUF_SIZE);
+    size_t datalen;
+
+    while (true)
+    {
+        if (xQueueReceive(uart_queue, &uart_event, portMAX_DELAY))
+        {
+            switch (uart_event.type)
+            {
+            case UART_DATA:
+                ESP_LOGI(TAG, "UART_DATA");
+                uart_read_bytes(UART_NUM_1, received_buffer, uart_event.size, portMAX_DELAY);
+                printf("received: %.*s\n", uart_event.size, received_buffer);
+                break;
+            case UART_BREAK:
+                ESP_LOGI(TAG, "UART_BREAK");
+                break;
+            case UART_BUFFER_FULL:
+                ESP_LOGI(TAG, "UART_BUFFER_FULL");
+                break;
+            case UART_FIFO_OVF:
+                ESP_LOGI(TAG, "UART_FIFO_OVF");
+                break;
+            case UART_PATTERN_DET:
+                ESP_LOGI(TAG, "UART_PATTERN_DET");
+                uart_get_buffered_data_len(UART_NUM_1, &datalen);
+                int pos = uart_pattern_pop_pos(UART_NUM_1);
+                ESP_LOGI(TAG, "Detected %d pos %d\n", datalen, pos);
+                uart_read_bytes(UART_NUM_1, received_buffer, datalen - PATTERNLEN, pdTICKS_TO_MS(100));
+                uint8_t pat[PATTERNLEN + 1];
+                memset(pat, 0, sizeof(pat));
+                uart_read_bytes(UART_NUM_1, pat, PATTERNLEN, pdTICKS_TO_MS(100));
+                printf("Data: %.*s === pattern: %s\n", datalen - PATTERNLEN, received_buffer, pat);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+#endif
+
+void app_main(void)
+{
+    uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE, TX_BUF_SIZE, 20, &uart_queue, 0);
+
+    uart_enable_pattern_det_baud_intr(UART_NUM_1, '+', PATTERN_LEN, 10000, 100, 100);
+    uart_pattern_queue_reset(UART_NUM_1, 20);
+    xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 10, NULL);
+
+    // char message[] = "anica i maja\n";
+    // printf("sending: %s\n", message);
+    // uart_write_bytes(UART_NUM_1, message, sizeof(message));
+
+    // char incoming_message[RX_BUF_SIZE];
+    // while(true)
+    // {
+    //     memset(incoming_message, 0, sizeof(incoming_message));
+    //     uart_read_bytes(UART_NUM_1, incoming_message, RX_BUF_SIZE, pdMS_TO_TICKS(5000));
+    //     printf("received: %s\n", incoming_message);
+    //     //uart_write_bytes(UART_NUM_1, message, sizeof(message));
+    // }
+}
