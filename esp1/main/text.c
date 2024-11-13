18a3884b9cc5d8c7d6272612c87bc7f9c646be78
Anica Gacevic <anicagacevic@gmail.com>
2024-11-13 12:28:13 +0100

    worksss

--- esp1/main/text.c
+++ esp1/main/text.c
@@ -13,7 +13,7 @@
 #include "esp_http_client.h"
 #include "wifi_connect.h"
 
-#define BUFFER_SIZE     500
+#define BUFFER_SIZE     1000
 #define HTTP_TAG "HTTP"
 
 // char *song_buffer; //[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
@@ -27,6 +27,9 @@ typedef struct song_buffer_t
 } song_buffer_t;
 
 uint8_t currentBuffer = 0;
+song_buffer_t song_buffer = {0};
+SemaphoreHandle_t buffer_ready[2];
+TaskHandle_t processing_buffer_handle = NULL;
 
 esp_err_t http_handler(esp_http_client_event_t *evt)
 {
@@ -43,60 +46,41 @@ esp_err_t http_handler(esp_http_client_event_t *evt)
         song_buffer_t *song_buffer = evt->user_data;
 
         uint32_t data_written = 0;
-
         while(data_written < evt->data_len)
         {
-            int remaining_space = BUFFER_SIZE - song_buffer->buffer_index;
-            ESP_LOGI(HTTP_TAG, "remaining: %d, data_len: %d, data_written: %lu, currentBuffer: %d",
-                                remaining_space, evt->data_len, data_written, currentBuffer);
-            
-            //calculate how many bytes to write to fill current buffer
-            int bytes_to_write = (evt->data_len - data_written < remaining_space) ? evt->data_len - data_written : remaining_space;
 
-            //write data to current buffer
-            memcpy(&song_buffer->buffer[currentBuffer][song_buffer->buffer_index], (char *)evt->data, bytes_to_write);
+            {
+                int remaining_space = BUFFER_SIZE - song_buffer->buffer_index;
+                ESP_LOGI(HTTP_TAG, "remaining: %d, data_len: %d, data_written: %lu, currentBuffer: %d",
+                                    remaining_space, evt->data_len, data_written, currentBuffer);
+                
+                //calculate how many bytes to write to fill current buffer
+                int bytes_to_write = (evt->data_len - data_written < remaining_space) ? evt->data_len - data_written : remaining_space;
 
-            //update buffer index 
-            song_buffer->buffer_index += bytes_to_write;
-            data_written += bytes_to_write;
-            ESP_LOGW(HTTP_TAG, "data_written: %lu, bytes_to_write: %d",
-                                data_written, bytes_to_write);
+                //write data to current buffer
+                memcpy(&song_buffer->buffer[currentBuffer][song_buffer->buffer_index], (char *)evt->data, bytes_to_write);
 
-            //switch between buffers
-            if(song_buffer->buffer_index == BUFFER_SIZE)
-            {
-                song_buffer->buffer_index = 0;
-                currentBuffer = (currentBuffer + 1) % 2;
+                //update buffer index 
+                song_buffer->buffer_index += bytes_to_write;
+                data_written += bytes_to_write;
+                ESP_LOGW(HTTP_TAG, "data_written: %lu, bytes_to_write: %d",
+                                    data_written, bytes_to_write);
+
+                //switch between buffers
+                if(song_buffer->buffer_index == BUFFER_SIZE)
+                {
+                    song_buffer->buffer_index = 0;
+
+                    xTaskNotifyGive(processing_buffer_handle);
+                    currentBuffer = (currentBuffer + 1) % 2;
+                }
+                //vTaskDelay(1000 /portTICK_PERIOD_MS);
             }
-            //vTaskDelay(1000 /portTICK_PERIOD_MS);
-        }
-#if 0
-        if (currentBuffer == 0) 
-        {
-            memcpy(&song_buffer->buffer0[song_buffer->buffer_index0], (char *)evt->data, evt->data_len);
-            song_buffer->buffer_index0 = song_buffer->buffer_index0 + evt->data_len;
-            song_buffer->buffer0[song_buffer->buffer_index0] = 0;
-            if (song_buffer->buffer_index0 + evt->data_len > BUFFER_SIZE) 
-            {
-                currentBuffer = 1;
-                song_buffer->buffer_index0 = 0;
-            }
-        }
-        else if (currentBuffer == 1) 
-        {
-            memcpy(&song_buffer->buffer1[song_buffer->buffer_index1], (char *)evt->data, evt->data_len);
-            song_buffer->buffer_index1 = song_buffer->buffer_index1 + evt->data_len;
-            song_buffer->buffer1[song_buffer->buffer_index1] = 0;
-            if (song_buffer->buffer_index1 + evt->data_len > BUFFER_SIZE) 
-            {
-                currentBuffer = 0;
-                song_buffer->buffer_index1 = 0
-            }
-        }
-#endif
+
         //printf("Message: %s\n", song_buffer->buffer0);
+        }
+        break;
     }
-    break;
     default:
     {
         ESP_LOGW(HTTP_TAG, "eventId: %d", evt->event_id);
@@ -105,7 +89,7 @@ esp_err_t http_handler(esp_http_client_event_t *evt)
     }
     return ESP_OK;
 }
-song_buffer_t song_buffer = {0};
+
 void get_song()
 {
     esp_http_client_config_t esp_http_client_config = {
@@ -113,12 +97,16 @@ void get_song()
         .method = HTTP_METHOD_GET,
         .event_handler = http_handler,
         .user_data = &song_buffer,
+        .buffer_size = BUFFER_SIZE,
         .timeout_ms = 1000,
     };
 
+    esp_err_t err = ESP_OK;
     esp_http_client_handle_t client = esp_http_client_init(&esp_http_client_config);
-    esp_err_t err = esp_http_client_perform(client);
-    
+    if(xSemaphoreTake(buffer_ready[currentBuffer], portMAX_DELAY) == pdTRUE)
+    {
+        err = esp_http_client_perform(client);
+    }
 
 
     if(err == ESP_OK)
@@ -129,17 +117,22 @@ void get_song()
     else{
         ESP_LOGI(HTTP_TAG, "GET request failed: %s\n", esp_err_to_name(err));
     }
-#if 0
-    if(song_buffer.buffer[0] != NULL)
-    {
-        free(song_buffer.buffer[0]);
-    }
-    if(song_buffer.buffer[1] != NULL)
-    {
-        free(song_buffer.buffer[1]);
-    }
-#endif
+
     esp_http_client_cleanup(client);
+}
+
+void processing_buffer(void * params)
+{
+    while(1) 
+    {
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    
+    int processBuffer = (currentBuffer + 1) % 2;
+    ESP_LOGI(HTTP_TAG, "Processing buffer %d...", processBuffer);
+    // funct here
+
+    xSemaphoreGive(buffer_ready[processBuffer]);
+    }
 }
 
 void get_song_task(void *pvParameters) 
@@ -160,5 +153,13 @@ void app_main(void)
     
     song_buffer.buffer[0] = calloc(BUFFER_SIZE, 1);
     song_buffer.buffer[1] = calloc(BUFFER_SIZE, 1);
+    buffer_ready[0] = xSemaphoreCreateBinary();
+    buffer_ready[1] = xSemaphoreCreateBinary();
+
+    xSemaphoreGive(buffer_ready[0]);
+    xSemaphoreGive(buffer_ready[1]);
+
     xTaskCreate(get_song_task, "get_song_task", 4096, NULL, 5, NULL);
+    
+    xTaskCreate(processing_buffer, "processing buffer", 4096, NULL, 5, &processing_buffer_handle);
 }
