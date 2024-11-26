999be401b0ff1e23e2d3b57dbc0311f50dc89167
Anica Gacevic <anicagacevic@gmail.com>
2024-11-26 09:31:31 +0100

    header file for text.c

---
+++ esp1/main/text.h
@@ -0,0 +1,18 @@
+#ifndef _TEXT_H_
+#define _TEXT_H_
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+extern void download_song(void);
